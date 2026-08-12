bd4c79766be8d08185f1a1efdcbbaa2e771ddbca
Greg Cobb <gcobb@vmware.com>
2026-08-12 15:40:15 -0700

    Revert back to last working bosh-lite pipeline

--- ci/bosh-lite/create-bosh-lite.sh
+++ ci/bosh-lite/create-bosh-lite.sh
@@ -6,8 +6,8 @@ set -eu
 : "${GCP_JSON_KEY:?}"
 
 # INPUTS
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-workspace_dir="$(cd "${script_dir}/../../.." && pwd)"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+workspace_dir="$( cd "${script_dir}/../../.." && pwd )"
 deployment_repo="${workspace_dir}/bosh-deployment"
 terraform_dir="${workspace_dir}/terraform"
 
@@ -17,7 +17,7 @@ state_dir="${workspace_dir}/director-state"
 env_name="$(cat "${workspace_dir}/terraform/name")"
 additional_args=''
 if [ -n "${GCP_INSTANCE_TYPE}" ]; then
-  cat >${script_dir}/custom-vm-size.yml <<EOD
+  cat > ${script_dir}/custom-vm-size.yml << EOD
 ---
 # Configure sizes for bosh-lite on gcp
 - type: replace
@@ -27,7 +27,7 @@ EOD
 
   additional_args="-o ${script_dir}/custom-vm-size.yml"
 elif [ -n "${GCP_INSTANCE_RAM}" ] && [ -n "${GCP_INSTANCE_CPU}" ]; then
-  cat >${script_dir}/custom-vm-size.yml <<EOD
+  cat > ${script_dir}/custom-vm-size.yml << EOD
 ---
 # Configure sizes for bosh-lite on gcp
 - type: replace
@@ -41,43 +41,40 @@ EOD
   additional_args="-o ${script_dir}/custom-vm-size.yml"
 fi
 
-pushd "${state_dir}" >/dev/null
-bosh interpolate "${deployment_repo}/bosh.yml" \
-  -o "${deployment_repo}/gcp/cpi.yml" \
-  -o "${deployment_repo}/bosh-lite.yml" \
-  -o "${deployment_repo}/bosh-lite-runc.yml" \
-  -o "${script_dir}/use-e2-standard-8.yml" \
-  -o "${deployment_repo}/jumpbox-user.yml" \
-  -o "${deployment_repo}/external-ip-not-recommended.yml" \
-  -o "${deployment_repo}/uaa.yml" \
-  -o "${deployment_repo}/external-ip-not-recommended-uaa.yml" \
-  -o "${deployment_repo}/credhub.yml" \
-  -o "${deployment_repo}/misc/source-releases/bosh.yml" \
-  -o "${script_dir}/use-external-ip-credhub.yml" ${additional_args} \
-  -o "${script_dir}/use-ssd-disks.yml" \
-  -o "${script_dir}/tag-bosh-lite-env.yml" \
-  -o "${script_dir}/use-jammy-stemcell-gcp-cpi.yml" \
-  -v director_name="bosh-lite" \
-  -v gcp_credentials_json="'${GCP_JSON_KEY}'" \
-  -v env_name="${env_name}" \
-  -l "${terraform_dir}/metadata" \
-  >./director.yml
+pushd "${state_dir}" > /dev/null
+  bosh interpolate "${deployment_repo}/bosh.yml" \
+    -o "${deployment_repo}/gcp/cpi.yml" \
+    -o "${deployment_repo}/bosh-lite.yml" \
+    -o "${deployment_repo}/bosh-lite-runc.yml" \
+    -o "${script_dir}/use-e2-standard-8.yml" \
+    -o "${deployment_repo}/jumpbox-user.yml" \
+    -o "${deployment_repo}/external-ip-not-recommended.yml" \
+    -o "${deployment_repo}/uaa.yml" \
+    -o "${deployment_repo}/external-ip-not-recommended-uaa.yml" \
+    -o "${deployment_repo}/credhub.yml" \
+    -o "${script_dir}/use-external-ip-credhub.yml" ${additional_args} \
+    -o "${script_dir}/use-ssd-disks.yml" \
+    -o "${script_dir}/tag-bosh-lite-env.yml" \
+    -v director_name="bosh-lite" \
+    -v gcp_credentials_json="'${GCP_JSON_KEY}'" \
+    -v env_name="${env_name}" \
+    -l "${terraform_dir}/metadata" \
+    > ./director.yml
 
-echo -e "\nDeploying new Bosh-Lite..."
-export BOSH_LOG_LEVEL=info
-bosh create-env \
-  --state ./state.json \
-  --vars-store ./creds.yml \
-  ./director.yml
+  echo -e "\nDeploying new Bosh-Lite..."
+  bosh create-env \
+    --state ./state.json \
+    --vars-store ./creds.yml \
+    ./director.yml
 
-export BOSH_ENVIRONMENT=$(bosh int director.yml --path=/instance_groups/name=bosh/networks/name=public/static_ips/0)
-export BOSH_CA_CERT="$(bosh interpolate --path /default_ca/ca creds.yml)"
-export BOSH_CLIENT="admin"
-export BOSH_CLIENT_SECRET=$(bosh int creds.yml --path=/admin_password)
+  export BOSH_ENVIRONMENT=$(bosh int director.yml --path=/instance_groups/name=bosh/networks/name=public/static_ips/0)
+  export BOSH_CA_CERT="$(bosh interpolate --path /default_ca/ca creds.yml)"
+  export BOSH_CLIENT="admin"
+  export BOSH_CLIENT_SECRET=$(bosh int creds.yml --path=/admin_password)
 
-echo -e "\nAdding bosh-dns via runtime config..."
-bosh update-runtime-config \
-  -n \
-  "${deployment_repo}/runtime-configs/dns.yml" \
-  --name=dns
-popd >/dev/null
+  echo -e "\nAdding bosh-dns via runtime config..."
+  bosh update-runtime-config \
+    -n \
+    "${deployment_repo}/runtime-configs/dns.yml" \
+    --name=dns
+popd > /dev/null
